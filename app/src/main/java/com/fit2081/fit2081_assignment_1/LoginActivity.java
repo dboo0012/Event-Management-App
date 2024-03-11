3c429d32706d2191f42a70eedf86ca0db8fe6c69
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2024-03-11 17:29:35 +1100

    add sharedPreferences save & load

--- app/src/main/java/com/fit2081/fit2081_assignment_1/LoginActivity.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/LoginActivity.java
@@ -3,23 +3,47 @@ package com.fit2081.fit2081_assignment_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    public static String LOG_KEY = "Launched";
+    EditText findUsername;
+    EditText findPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Assign the username and password values
+        findUsername = findViewById(R.id.loginUsername);
+        findPassword = findViewById(R.id.loginPassword);
+
+        // Load shared preferences here to prefill username
+        getDataFromSharedPreferences();
+
+        // Debugging
+        Log.d(LOG_KEY, "launched main activity");
+    }
+
+    public void getDataFromSharedPreferences(){
+        // initialise shared preference class variable to access Android's persistent storage
+        SharedPreferences sharedPreferences = getSharedPreferences("DatabaseStore.java", MODE_PRIVATE);
+
+        // save key-value pairs to the shared preference file
+        String studentNameRestored = sharedPreferences.getString(DatabaseStore.KEY_USERNAME, "");
+
+        // update the UI using retrieved values
+        findUsername.setText(studentNameRestored);
     }
 
     public void loginButtonOnClick(View view) {
-        EditText findUsername = findViewById(R.id.loginUsername);
+        // Parsing the username and password
         String username = findUsername.getText().toString();
-        EditText findPassword = findViewById(R.id.loginPassword);
         String password = findPassword.getText().toString();
 
         // Verification process
@@ -39,7 +63,6 @@ public class LoginActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "Oops! Username or Password incorrect.", Toast.LENGTH_SHORT).show();
                 // Reset the username and password fields
-//                findUsername.setText("");
                 findPassword.setText("");
             }
         }
--- app/src/main/java/com/fit2081/fit2081_assignment_1/MainActivity.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/MainActivity.java
@@ -3,17 +3,43 @@ package com.fit2081.fit2081_assignment_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static String LOG_KEY = "Launched";
+    EditText findUsername;
+    EditText findPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(LOG_KEY, "launched main activity");
+
+        // Assign the username and password values
+        findUsername = findViewById(R.id.signUpUsername);
+        findPassword = findViewById(R.id.signUpPassword);
+    }
+
+    public void signUpButtonOnClick(View view){
+        String username = findUsername.getText().toString();
+        String password = findPassword.getText().toString();
+
+        // Check if both fields are filled
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill both username and password fields", Toast.LENGTH_SHORT).show();
+        } else {
+            saveDataToSharedPreferences(username, password);
+            String out = String.format("Successfully registered %s!", username);
+            Toast.makeText(this, out, Toast.LENGTH_SHORT).show();
+            // Go to login page
+            launchLogin(view);
+        }
     }
 
     public void launchLogin(View view){
@@ -21,20 +47,15 @@ public class MainActivity extends AppCompatActivity {
         startActivity(loginIntent);
     }
 
-    public void signUpButtonOnClick(View view){
-        EditText findUsername = findViewById(R.id.signUpUsername);
-        String username = findUsername.getText().toString();
-        EditText findPassword = findViewById(R.id.signUpPassword);
-        String password = findPassword.getText().toString();
-
-        // Check if both fields are filled
-        if (username.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Please fill both username and password fields", Toast.LENGTH_SHORT).show();
-        } else {
-            String out = String.format("Successfully registered %s!", username);
-            Toast.makeText(this, out, Toast.LENGTH_SHORT).show();
-            // Go to login page
-            launchLogin(view);
-        }
+    public void saveDataToSharedPreferences(String username, String password){
+        // Shared prefereces (test ONLY)
+        SharedPreferences sharedPreferences = getSharedPreferences("DatabaseStore.java", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        // Create a static key
+        editor.putString(DatabaseStore.KEY_USERNAME, username);
+        editor.putString(DatabaseStore.KEY_PASSWORD, password);
+        editor.apply();
+        // Do set text to save previous
+//        findUsername.setText(username);
     }
 }

6ea8eebfae9a3dc42f64fb9e0447e3b3ad3cbca3
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2024-03-13 13:33:10 +1100

    add event category and event activities

--- app/src/main/java/com/fit2081/fit2081_assignment_1/DashboardActivity.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/DashboardActivity.java
@@ -2,7 +2,9 @@ package com.fit2081.fit2081_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    private void launchIntent(Class<?> targetClass){
+        // Creates a new intent instance to the target activity class and launch it
+        Intent newIntent = new Intent(this, targetClass);
+        startActivity(newIntent);
+    }
+
+    public void eventCategoryButtonOnClick(View view){
+        launchIntent(EventCategoryActivity.class);
+    }
+
+    public void addEventButtonOnClick(View view){
+        launchIntent(EventActivity.class);
     }
 }
---
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/EventCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.fit2081.fit2081_assignment_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_category);
+    }
+}

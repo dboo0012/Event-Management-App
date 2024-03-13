77c1eb01ba64e820ee14a74bbc096e7387d96095
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2024-03-13 14:14:25 +1100

    create event category methods

--- app/src/main/java/com/fit2081/fit2081_assignment_1/EventCategoryActivity.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/EventCategoryActivity.java
@@ -2,13 +2,49 @@ package com.fit2081.fit2081_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class EventCategoryActivity extends AppCompatActivity {
+    String key = MainActivity.LOG_KEY;
+    EditText findEventName;
+    EditText findEventCount;
+    Switch isEventActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_category);
+
+        // Assign the attributes
+        findEventName = findViewById(R.id.et_categoryName);
+        findEventCount = findViewById(R.id.et_eventCount);
+        isEventActive = findViewById(R.id.switch_isCategoryActive);
+
+        // Debugging
+        Log.d(key, "launched main activity");
     }
+
+    public void createEventCategoryButtonOnClick(View view){
+        // form validation
+        // check that event category name is filled
+    }
+
+    public void saveAttributesToSharedPreferences(String username, String password){
+        // Initialise shared preference class variable to access persistent storage
+        SharedPreferences sharedPreferences = getSharedPreferences("DatabaseStore.java", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit(); // Open the shared preference editor
+
+        // Update the values of the shared preference
+        editor.putString(DatabaseStore.KEY_USERNAME, username);
+        editor.putString(DatabaseStore.KEY_PASSWORD, password);
+        editor.apply();
+
+//        findUsername.setText(username);
+    }
+
 }

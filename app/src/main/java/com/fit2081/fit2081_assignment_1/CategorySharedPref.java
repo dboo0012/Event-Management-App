b25c09fa3220f0bf36045fce53a70d8cb009398a
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2024-03-13 16:17:35 +1100

    activity formatting

--- app/src/main/java/com/fit2081/fit2081_assignment_1/CategorySharedPref.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/CategorySharedPref.java
@@ -1,8 +1,8 @@
 package com.fit2081.fit2081_assignment_1;
 
 public class CategorySharedPref {
-    public static String categoryId = "KEY_CATEGORY_ID"; // do for all
-    public static String categoryName;
-    public static String eventCount;
-    public static String isActive;
+    public static String KEY_CATEGORY_ID = "KEY_CATEGORY_ID";
+    public static String KEY_CATEGORY_NAME = "KEY_CATEGORY_NAME";
+    public static String KEY_EVENT_COUNT = "KEY_CATEGORY_NAME";
+    public static String KEY_IS_ACTIVE = "KEY_CATEGORY_NAME";
 }
--- app/src/main/java/com/fit2081/fit2081_assignment_1/EventCategoryActivity.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/EventCategoryActivity.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EventCategoryActivity extends AppCompatActivity {
     String key = MainActivity.LOG_KEY;
+    TextView findCategoryId;
     EditText findCategoryName;
     EditText findEventCount;
     Switch findIsCategoryActive;
@@ -24,10 +26,14 @@ public class EventCategoryActivity extends AppCompatActivity {
         // Generate category ID (with method)
 
         // Assign the attributes
+//        findCategoryId = findViewById(R.id.tv_categoryId);
         findCategoryName = findViewById(R.id.et_categoryName);
         findEventCount = findViewById(R.id.et_eventCount);
         findIsCategoryActive = findViewById(R.id.switch_isCategoryActive);
 //        findIsCategoryActive.setChecked(false);
+
+        // set category id field to default every time
+//        findCategoryId.setText("(auto generated upon creation");
 
         // Debugging
         Log.d(key, "launched event category activity");
@@ -36,19 +42,35 @@ public class EventCategoryActivity extends AppCompatActivity {
     public void createEventCategoryButtonOnClick(View view){
         // Parse the values
         String categoryName = findCategoryName.getText().toString();
-        int eventCount = Integer.parseInt(findEventCount.getText().toString());
         boolean isCategoryActive = findIsCategoryActive.isChecked();
+        int eventCount;
+
+        // TO BE VERIFIED
+        try{
+            eventCount = Integer.parseInt(findEventCount.getText().toString());
+        } catch (Exception e){
+            eventCount = 0;
+        }
 
         // form validation
         if (categoryName.isEmpty()){ // check that event category name is filled
             Toast.makeText(this, "Event category name required", Toast.LENGTH_SHORT).show();
+            Log.d(key, "name not entered");
         } else {
-//            saveAttributesToSharedPreferences(<?>,categoryName, eventCount, isCategoryActive);
+            String categoryId = generateCategoryID();
 
-            String out = "Category saved successfully: CME-1084"; // Show event category ID
+            saveAttributesToSharedPreferences(categoryId, categoryName, eventCount, isCategoryActive);
+
+            String out = String.format("Category saved successfully: %s", categoryId); // Show event category ID
             Toast.makeText(this, out, Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private String generateCategoryID(){
+        String id = "CME-1084";
+
+        return id;
     }
 
     public void saveAttributesToSharedPreferences(String categoryId, String categoryName, int eventCount, boolean isActive){
@@ -57,11 +79,12 @@ public class EventCategoryActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit(); // Open the shared preference editor
 
         // Add all attributes to SharedPreferences
-        editor.putString(CategorySharedPref.categoryId, categoryId);
-        editor.putString(CategorySharedPref.categoryName, categoryName);
-        editor.putInt(CategorySharedPref.eventCount, eventCount);
-        editor.putBoolean(CategorySharedPref.isActive, isActive);
+        editor.putString(CategorySharedPref.KEY_CATEGORY_ID, categoryId);
+        editor.putString(CategorySharedPref.KEY_CATEGORY_NAME, categoryName);
+        editor.putInt(CategorySharedPref.KEY_EVENT_COUNT, eventCount);
+        editor.putBoolean(CategorySharedPref.KEY_IS_ACTIVE, isActive);
 
+        // Apply the changes
         editor.apply();
     }
 }

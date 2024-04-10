3f899574376a58043eb114c192e3538917d3d4ed
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2024-04-10 11:55:49 +1000

    Implemented navigation drawer

--- app/src/main/java/com/fit2081/fit2081_assignment_1/activities/DashboardActivity.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/activities/DashboardActivity.java
@@ -1,8 +1,10 @@
 package com.fit2081.fit2081_assignment_1.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,18 +15,35 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.fit2081.fit2081_assignment_1.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class DashboardActivity extends AppCompatActivity {
     String key = MainActivity.LOG_KEY;
+    Toolbar toolbar;
+    NavigationView navView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app_bar_layout);
+        setContentView(R.layout.drawer_layout);
 
         // Toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // Using the toolbar as action bar
+
+        // Nav drawer
+        navView = findViewById(R.id.nav_view);
+        NavigationHandler navigationHandler = new NavigationHandler();
+        navView.setNavigationItemSelectedListener(navigationHandler); // Listens to navigation item selection
+
+        // Sync
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         // Debugging
         Log.d(key, "launched dashboard activity");
@@ -66,4 +85,22 @@ public class DashboardActivity extends AppCompatActivity {
     public void addEventButtonOnClick(View view){
         launchIntent(EventActivity.class);
     }
+
+    class NavigationHandler implements NavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int itemId = item.getItemId();
+            if (itemId == R.id.drawer_view_category) {
+                Snackbar.make(navView, "View Category", Snackbar.LENGTH_SHORT).show();
+            } else if (itemId == R.id.drawer_add) {
+                Snackbar.make(navView, "Add category", Snackbar.LENGTH_SHORT).show();
+            } else if (itemId == R.id.drawer_view_events) {
+                Snackbar.make(navView, "View All Events", Snackbar.LENGTH_SHORT).show();
+            } else if (itemId == R.id.drawer_logout) {
+                Snackbar.make(navView, "Logout", Snackbar.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    }
+
 }

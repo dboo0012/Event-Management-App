747ca49693585a2ed055a2beeccd9b602c656ef3
dboo0012 <115628797+dboo0012@users.noreply.github.com>
2024-04-15 11:47:21 +1000

    changed colours

--- app/src/main/java/com/fit2081/fit2081_assignment_1/fragments/FragmentEventForm.java
+++ app/src/main/java/com/fit2081/fit2081_assignment_1/fragments/FragmentEventForm.java
@@ -87,7 +87,7 @@ public class FragmentEventForm extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_form, container, false);
         // Assign the attributes to its respective views
         findEventId = view.findViewById(R.id.tv_eventIdValue);
         findCategoryId = view.findViewById(R.id.et_eventCategoryId);
